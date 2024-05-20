e9876ef6b59c9ce09c5880b82da9009a152fdb61
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-05-20 16:50:22 +0530

    Add files via upload

---
+++ Package_example.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//.......................PACKAGE EXAMPLE...........................
+//This is user defined package
+package Oops_concept;
+//This is InBuilt package
+import java.util.*;
+public class Package_example {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a);
+    }
+}
